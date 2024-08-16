5737bd831271cc1a98673135614e3922fba99a0f
Jimmy Cassis <KernelOops@outlook.com>
2024-08-16 11:03:30 -0700

    Revert menu text change into array

--- vis_milk2/plugin.h
+++ vis_milk2/plugin.h
@@ -732,7 +732,7 @@ class CPlugin : public CPluginShell
     TextElement m_songTitle;
     TextElement m_songStats;
     TextElement m_waitText;
-    TextElement m_menuText[MAX_PRESETS_PER_PAGE];
+    TextElement m_menuText;
     TextElement m_loadPresetInstruction;
     TextElement m_loadPresetDir;
     TextElement m_loadPresetItem[MAX_PRESETS_PER_PAGE];
