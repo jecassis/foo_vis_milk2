a2288817597b232c889e6d4b932ba1ffdfb9d907
Jimmy Cassis <KernelOops@outlook.com>
2024-09-02 05:28:58 -0700

    Attempt to fix test DLL to query the DUT data

--- test/pch.h
+++ test/pch.h
@@ -15,75 +15,25 @@
 #endif
 #include <sdkddkver.h>
 
-#define NOBITMAP
+#define NOSYSMETRICS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define OEMRESOURCE
+#define NOATOM
+#define NOKERNEL
+#define NOMEMMGR
 #define NOMINMAX
-#define NOMCX
+#define NOOPENFILE
 #define NOSERVICE
-
+#define NOSOUND
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOPROFILER
+#define NOMCX
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
 #include <helpers/foobar2000-lite+atl.h>
-
-class foobar2000_component_globals
-{
-  public:
-    foobar2000_component_globals()
-    {
-#if defined(_MSC_VER) && !defined(_DEBUG) && !defined(_DLL)
-        ::OverrideCrtAbort();
-#endif
-    }
-};
-
-class NOVTABLE foobar2000_client
-{
-  public:
-    typedef service_factory_base* pservice_factory_base;
-
-    enum
-    {
-        FOOBAR2000_CLIENT_VERSION_COMPATIBLE = FOOBAR2000_TARGET_VERSION_COMPATIBLE,
-        FOOBAR2000_CLIENT_VERSION = FOOBAR2000_TARGET_VERSION,
-    };
-    virtual t_uint32 get_version() = 0;
-    virtual pservice_factory_base get_service_list() = 0;
-
-    virtual void get_config(stream_writer* p_stream, abort_callback& p_abort) = 0;
-    virtual void set_config(stream_reader* p_stream, abort_callback& p_abort) = 0;
-    virtual void set_library_path(const char* path, const char* name) = 0;
-    virtual void services_init(bool val) = 0;
-    virtual bool is_debug() = 0;
-
-  protected:
-    foobar2000_client() {}
-    ~foobar2000_client() {}
-};
-
-class NOVTABLE foobar2000_api
-{
-  public:
-    virtual service_class_ref service_enum_find_class(const GUID& p_guid) = 0;
-    virtual bool service_enum_create(service_ptr_t<service_base>& p_out, service_class_ref p_class, t_size p_index) = 0;
-    virtual t_size service_enum_get_count(service_class_ref p_class) = 0;
-    virtual fb2k::hwnd_t get_main_window() = 0;
-    virtual bool assert_main_thread() = 0;
-    virtual bool is_main_thread() = 0;
-    virtual bool is_shutting_down() = 0;
-    virtual const char* get_profile_path() = 0;
-    virtual bool is_initializing() = 0;
-
-    //New in 0.9.6
-    virtual bool is_portable_mode_enabled() = 0;
-    virtual bool is_quiet_mode_enabled() = 0;
-
-  protected:
-    foobar2000_api() {}
-    ~foobar2000_api() {}
-};
-
-static bool g_services_available = false, g_initialized = false;
-extern foobar2000_api* g_foobar2000_api;
+#include <sdk/component.h>
 
 #ifdef _WIN32
 #define FOOBAR2000_CC __cdecl
@@ -108,9 +58,9 @@ p_foobar2000_client FOOBAR2000_API foobar2000_get_interface(foobar2000_api* p_ap
 class NOVTABLE foobar2000_api_impl : public foobar2000_api
 {
   public:
-    service_class_ref service_enum_find_class(const GUID& p_guid) { return 0; }
+    service_class_ref service_enum_find_class(const GUID& p_guid) { return reinterpret_cast<void*>(static_cast<size_t>(rand())); }
     bool service_enum_create(service_ptr_t<service_base>& p_out, service_class_ref p_class, t_size p_index) { return false; }
-    t_size service_enum_get_count(service_class_ref p_class) { return 0; }
+    t_size service_enum_get_count(service_class_ref p_class) { return 5; }
     fb2k::hwnd_t get_main_window() { return NULL; };
     bool assert_main_thread() { return false; }
     bool is_main_thread() { return false; }
@@ -125,11 +75,11 @@ namespace core_api
 {
 bool are_services_available()
 {
-    return g_services_available;
+    return true;
 }
 
 bool is_main_thread()
 {
-    return (g_services_available && g_foobar2000_api) ? g_foobar2000_api->is_main_thread() : true;
+    return true;
 }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -87,8 +87,9 @@ TEST_CLASS(Milk2DllTest)
     {
         Logger::WriteMessage("Smoke DLL Load Test\n");
 
+        static const GUID guid_ui_element_milk2 = { 0x0B52C703, 0x1586, 0x42F7, {0xA8, 0x4C, 0x70, 0x54, 0xCD, 0xC8, 0x22, 0x55} }; // {0B52C703-1586-42F7-A84C-7054CDC82255} foo_vis_milk2.dll!void(* service_factory_single_t<ui_element_milk2>::`vftable'[2])()
         static const GUID guid_milk2 = { 0x204b0345, 0x4df5, 0x4b47, {0xad, 0xd3, 0x98, 0x9f, 0x81, 0x1b, 0xd9, 0xa5} }; // {204B0345-4DF5-4B47-ADD3-989F811BD9A5}
-        static const GUID preferences_page_guid = { 0x7255e8d0, 0x3fcf, 0x4781, {0xb9, 0x3b, 0xd0, 0x6c, 0xb8, 0x8d, 0xfa, 0xfa} }; // {7255E8D0-3FCF-4781-B93B-D06CB88DFAFA}
+        static const GUID preferences_page_guid = { 0x7255e8d0, 0x3fcf, 0x4781, {0xb9, 0x3b, 0xd0, 0x6c, 0xb8, 0x8d, 0xfa, 0xfa} }; // {7255E8D0-3FCF-4781-B93B-D06CB88DFAFA} foo_vis_milk2.dll!void(* preferences_page_factory_t<preferences_page_milk2>::`vftable'[2])()
         static const GUID initquit_guid = { 0x113773c4, 0xb387, 0x4b48, {0x8b, 0xdf, 0xab, 0x58, 0xbc, 0x6c, 0xe5, 0x38} }; // {113773C4-B387-4b48-8BDF-AB58BC6CE538}
         static const GUID advconfig_entry_guid = { 0x7e84602e, 0xdc49, 0x4047, {0xaa, 0xee, 0x63, 0x71, 0x8b, 0xbc, 0x5a, 0x1f} }; // {7E84602E-DC49-4047-AAEE-63718BBC5A1F}
         static const GUID componentversion_guid = { 0x10bb3ebd, 0xddf7, 0x4975, {0xa3, 0xcc, 0x23, 0x8, 0x48, 0x29, 0x45, 0x3e} }; // {10BB3EBD-DDF7-4975-A3CC-23084829453E}
@@ -101,13 +102,13 @@ TEST_CLASS(Milk2DllTest)
         wx.lpszClassName = class_name;
         Assert::IsNotNull(wx.hInstance);
 
-        foobar2000_api_impl* papi = new foobar2000_api_impl();
+        g_foobar2000_api = new foobar2000_api_impl();
 
         RegisterClassEx(&wx);
         HWND hWnd = CreateWindowEx(0, class_name, L"Message Window", 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, NULL, NULL);
         Assert::IsNotNull(hWnd);
 
-        foobar2000_client* component = foobar2000_get_interface(papi, wx.hInstance);
+        foobar2000_client* component = foobar2000_get_interface(g_foobar2000_api, wx.hInstance);
         Assert::IsNotNull(component);
 
         uint32_t ver = component->get_version();
@@ -118,8 +119,14 @@ TEST_CLASS(Milk2DllTest)
         component->set_config(rs, fb2k::noAbort);
         component->set_library_path("path", "test");
         component->services_init(true);
+        bool vis = serv->is_service_present(guid_milk2);
+        bool prefs = serv->is_service_present(preferences_page_guid);
+        bool iq = serv->is_service_present(initquit_guid);
         bool debug = component->is_debug();
 
+        Assert::IsTrue(vis, L"Visualization UI element service not installed.");
+        Assert::IsTrue(prefs, L"Preferences page not installed.");
+        Assert::IsTrue(iq, L"InitQuit not installed.");
         Assert::AreEqual(static_cast<uint32_t>(FOOBAR2000_TARGET_VERSION), ver, L"foobar2000 component client version mismatches");
 #ifdef _DEBUG
         Assert::IsTrue(debug, L"foobar2000 component client is not in debug mode.");
